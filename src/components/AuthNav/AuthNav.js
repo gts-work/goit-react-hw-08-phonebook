4d6fb17c6a7c5701aa9de2d67a2ba7784b7ef99e
Grygorii Tsaryk <artgrem@gmail.com>
2021-09-01 20:16:19 +0300

    added stles for auth nav

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -2,6 +2,8 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { Nav, Button } from "react-bootstrap";
 
+import styles from "./AuthNav.module.css";
+
 const AUTH_NAV_TITLES = [
   { id: "t3", title: "Sign in", url: "/login" },
   { id: "t4", title: "Join us", url: "/register" },
@@ -9,12 +11,12 @@ const AUTH_NAV_TITLES = [
 
 export default function AuthNav() {
   return (
-    <Nav className="justify-content-end">
+    <Nav className={styles.authNav}>
       {AUTH_NAV_TITLES.map((nav) => {
         const { id, url, title } = nav;
 
         return (
-          <Nav.Item key={id}>
+          <Nav.Item key={id} className={styles.authNavItem}>
             <NavLink exact eventKey={id} to={url}>
               <Button variant="outline-primary">{title}</Button>{" "}
             </NavLink>
